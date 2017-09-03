303589365855a4332350fff80890017afb6368df
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-09-03 20:19:22 +0000

    Reposition cursor to next line before typing

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ let TypingElement = null;
 let InvalidCodePath = () => { Assert(!"InvalidCodePath"); }
 
 let Assert = ( expression ) => {
-  if (!(expression))
-    throw "Failed Assertion";
+  if (!(expression)) {
+    throw new Error("Failed Assertion").stack;
+
+  }
 }
 
 let PageLoaded = () => {
@@ -22,39 +24,27 @@ let PageLoaded = () => {
 }
 
 let WaitTillLoaded = setInterval( () => {
-  SetVisibility(document.body, VISIBILITY_OFF);
+  document.body.style.visibility = VISIBILITY_OFF;
   if ( PageLoaded() ) {
     clearInterval(WaitTillLoaded);
     Init().then ( (typedElements) => Main(typedElements) );
   }
 }, 5);
 
-// let BindCallback = (id, event, callback) => {
-//   Assert( typeof(id) === "string" );
-//   Assert( typeof(event) === "string" );
-//   Assert( typeof(callback) === "function" );
+function TypedElement(Elem) {
+  this.Content = Elem.innerHTML.split("");
+  this.FinalBounds = Elem.getBoundingClientRect();
 
-//   let Elem = document.getElementById(id);
-//   console.log(Elem);
-//   Elem['on' + event] = callback;
-// }
-
-let TypedElement = (Elem) => {
-  let Result = {}
-
-  Result.Content = Elem.innerHTML.split("");
-  Result.FinalBounds = Elem.getBoundingClientRect();
-
-  Result.DomElem = Elem;
-
-  return Result;
+  this.DomElem = Elem;
 }
 
 let SetVisibility = (element, vis) => {
+  Assert(element instanceof HTMLElement);
   element.style.visibility = vis;
 }
 
 let ToggleDisplay = (element, d1, d2) => {
+  Assert(element instanceof HTMLElement);
   let display = window.getComputedStyle(element).display;
 
   if (display === d1) {
@@ -67,6 +57,7 @@ let ToggleDisplay = (element, d1, d2) => {
 }
 
 let SetDisplay = (element, display) => {
+  Assert(element instanceof HTMLElement);
   element.style.display = display;
 }
 
@@ -118,6 +109,7 @@ let setCursorP = (Cursor, {x, y}) => {
 }
 
 let UpdateCursor = (Cursor, Elem) => {
+  Assert(Elem instanceof TypedElement);
   let bounds = Elem.DomElem.getBoundingClientRect();
 
   let height = bounds.bottom - bounds.top;
@@ -133,32 +125,42 @@ let UpdateCursor = (Cursor, Elem) => {
 }
 
 let typeText = (Elem, Cursor, finalDelay = 500) => {
+  Assert(Elem instanceof TypedElement);
+  let charInterval = 50;
 
   return new Promise( (resolve, reject) => {
-    let text = Elem.Content;
-    Elem.DomElem.innerHTML = "";
+      PrepareToType(Cursor, Elem);
+      setTimeout( () => {
+        resolve();
+      }, charInterval );
 
-    SetDisplay(Elem.DomElem, DISPLAY_INLINE_BLOCK);
+  }).then( () => {
 
-    let anim = setInterval( () => {
-      if (text.length == 0) {
-        setTimeout( () => {
-          SetVisibility(Cursor, VISIBILITY_ON);
-          clearInterval(anim);
-          resolve();
-        }, finalDelay );
+    return new Promise( (resolve, reject) => {
 
-      } else {
-        Elem.DomElem.innerHTML += text.shift();
-        UpdateCursor(Cursor, Elem);
-      }
+      SetVisibility(Elem.DomElem, VISIBILITY_ON);
 
-    }, 50 );
+      let text = Elem.Content;
+      Elem.DomElem.innerHTML = "";
+
+      let anim = setInterval( () => {
+
+        if (text.length == 0) {
+          setTimeout( () => { clearInterval(anim); resolve(); }, finalDelay );
+
+        } else {
+          Elem.DomElem.innerHTML += text.shift();
+          UpdateCursor(Cursor, Elem);
+        }
+
+      }, charInterval );
+    });
 
   });
 }
 
 let RewindCursor = (Cursor, Elem) => {
+  Assert(Elem instanceof TypedElement);
   let bounds = Elem.DomElem.getBoundingClientRect();
 
   let x = bounds.left;
@@ -167,7 +169,19 @@ let RewindCursor = (Cursor, Elem) => {
   setCursorP(Cursor, {x,y} );
 }
 
+let SetHeight = (DomElem, Bounds) => {
+  Assert(DomElem instanceof HTMLElement);
+  DomElem.style.height = Bounds.bottom - Bounds.top;;
+}
+
 let PrepareToType = (Cursor, Elem) => {
+  Assert(Elem instanceof TypedElement);
+
+  SetHeight(Elem.DomElem, Elem.FinalBounds);
+
+  SetDisplay(Elem.DomElem, DISPLAY_INLINE_BLOCK);
+  SetVisibility(Elem.DomElem, VISIBILITY_OFF);
+
   UpdateCursor(Cursor, Elem);
   RewindCursor(Cursor, Elem);
 }
@@ -180,7 +194,7 @@ let Init = () => {
     let content = Array.from(document.getElementsByClassName("content"));
     let Cursor = document.getElementById("cursor");
 
-    let firstElem = TypedElement(typedElements[0]);
+    let firstElem = new TypedElement(typedElements[0]);
 
     let mainElements = [];
 
@@ -191,8 +205,9 @@ let Init = () => {
           ++Index)
     {
       let Elem = typedElements[Index];
+      mainElements.push(new TypedElement(Elem));
+
       SetDisplay(Elem, DISPLAY_NONE);
-      mainElements.push(TypedElement(Elem));
     }
 
     for ( let Index = 0;
@@ -203,36 +218,36 @@ let Init = () => {
       SetDisplay(Elem, DISPLAY_NONE);
     }
 
-    SetVisibility(document.body, VISIBILITY_ON);
+    document.body.style.visibility = VISIBILITY_ON;
 
     resolve(mainElements);
   });
 }
 
-let Main = (typedElements) => {
+let Main = (elements) => {
 
   let Cursor = document.getElementById("cursor");
 
-  blinkCursor(Cursor, 5).then( () => {
-    return typeText(typedElements[0], Cursor);
+  blinkCursor(Cursor, 1).then( () => {
+    return typeText(elements[0], Cursor);
   }).then ( () => {
-    return typeText(typedElements[5], Cursor, 500);
+    return typeText(elements[5], Cursor, 500);
   }).then( () => {
-    return typeText(typedElements[1], Cursor);
+    return typeText(elements[1], Cursor);
   }).then( () => {
-    return typeText(typedElements[2], Cursor);
+    return typeText(elements[2], Cursor);
   }).then ( () => {
-    return typeText(typedElements[3], Cursor);
+    return typeText(elements[3], Cursor);
   }).then ( () => {
-    return typeText(typedElements[4], Cursor, 0);
+    return typeText(elements[4], Cursor, 0);
   }).then ( () => {
     return blinkCursor(Cursor, 1);
   }).then ( () => {
-    return typeText(typedElements[6], Cursor, 0);
+    return typeText(elements[6], Cursor, 0);
   }).then ( () => {
     return blinkCursor(Cursor, 1);
   }).then ( () => {
-    return typeText(typedElements[7], Cursor, 300);
+    return typeText(elements[7], Cursor, 300);
   })
 
 
