2a79eb095cb8b251671fa4edfb11ab66ca6adb5c
Jesse Hughes <jesse.hughes.it@gmail.com>
2022-09-14 13:25:57 -0600

    Make all script includes absolute

--- make.sh
+++ make.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-aws s3 sync --acl "public-read" --delete -- ./docs s3://scallywag.software
+# aws s3 sync --acl "public-read" --delete -- ./docs s3://scallywag.software
 
+cp docs/index.html docs/404.html && git add . && git commit -av
