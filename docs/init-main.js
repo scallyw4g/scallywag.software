2266cdb711f9e42fd7716e4e4eb451ffeb6b6186
Jesse Hughes <jesse.hughes.it@gmail.com>
2023-01-05 16:13:01 -0800

    Add thread pinning blog post

--- docs/init-main.js
+++ docs/init-main.js
@@ -21,6 +21,7 @@ const ROUTE_VIM_RESUME  = "/vim/resume";
 const ROUTE_VIM_BLOG                        = "/vim/blog";
 const ROUTE_VIM_BLOG_IMPROVE_TEXT_RENDERING = "/vim/blog/improving-text-rendering";
 const ROUTE_VIM_BLOG_PROFILING_CONTEXT_SWITCHES = "/vim/blog/profiling-context-switches-with-ETW";
+const ROUTE_VIM_BLOG_THREAD_PINNING_ON_WINDOWS  = "/vim/blog/thread-pinning-on-windows";
 
 const INTRO_ANIM_COMPLETE = "IntroAnimationComplete"
 const INDEX_ANIM_COMPLETE = "IndexAnimationComplete"
